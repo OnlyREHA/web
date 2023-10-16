45d142cb340327ee0e0c2031a2202c1000c8e97e
OnlyREHA <145514740+OnlyREHA@users.noreply.github.com>
2023-10-16 17:35:54 +0900

    Add files via upload

---
+++ 222/js/main.js
@@ -0,0 +1,43 @@
+
+
+// 메뉴
+$('.nav>ul>li').mouseover(function(){
+    $(this).find('.submenu').stop().slideDown();
+}).mouseout(function(){
+    $(this).find('.submenu').stop().slideUp();
+});
+
+
+//슬라이드
+let currentIndex=0;
+
+setInterval(function(){
+    if(currentIndex<2){  
+        currentIndex++;   //조건문이 성립할때 실행
+    }else{  
+        currentIndex=0;   //조건문이 성립하지 않을때 실행
+    }
+    
+    let slidePosition=currentIndex * (-1000)+"px"
+    console.log(slidePosition)
+
+    $('.slidelist').animate({
+        left:slidePosition
+    },1000)
+},3000);  //3초마다 어떤일이 일어난다
+
+
+
+//팝업
+$('#content1 .right').click(function(){
+    $('.layer_bg').fadeIn();
+});
+
+$('.layer_bg .layer .close').click(function(){
+    $('.layer_bg').hide();
+});
+
+//window창 열기
+$('.contact').click(function(){
+    window.open("contact.html", "_child","top=0,left=0,width=400,height=400");
+})
